2693a0c9dcac86b0c7668e029509ac89375e1bf3
igoryen <igor.yentaltsev@gmail.com>
2014-02-11 17:15:29 -0500

    Models/DataContext.cs: added; Models/Initializer.cs: modified

---
+++ Lab5/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace CodeFirstOne.Models {
+  public class DataContext : DbContext {
+    public DataContext() : base("name=DataContext") { }
+
+    public DbSet<A> As { get; set; }
+    public DbSet<O> Os { get; set; }
+    public DbSet<E> Es { get; set; }
+
+    public System.Data.Entity.DbSet<CodeFirstOne.ViewModels.aaaFull> aaaFulls { get; set; }
+  }
+}
--- Lab5/Models/Initiallizer.cs
+++ Lab5/Models/Initiallizer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using Lab5.Controllers;
 
 namespace Lab5.Models {
 
