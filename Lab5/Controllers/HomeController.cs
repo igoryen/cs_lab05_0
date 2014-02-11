acbb2ab99bf2aeb000d344ab9338f8566c02e7a3
igoryen <igor.yentaltsev@gmail.com>
2014-02-11 17:01:56 -0500

    Controllers, Models: formatted

--- Lab5/Controllers/HomeController.cs
+++ Lab5/Controllers/HomeController.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Lab5.Controllers
-{
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
+namespace Lab5.Controllers {
+
+  public class HomeController : Controller {
+
+    public ActionResult Index() {
+      return View();
     }
+  }
 }
--- Lab5/Models/AppDomainClasses.cs
+++ Lab5/Models/AppDomainClasses.cs
@@ -4,60 +4,55 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 
-namespace Lab5.Models
-{
-    public class Movie
-    {
-        public Movie()
-        {
-            this.Genres = new List<Genre>();
-        }
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string Title { get; set; }
-        [Required]
-        public decimal TicketPrice { get; set; }
-        
-        public Director Director { get; set; }
-        
-        public List<Genre> Genres { get; set; }
-    }
+namespace Lab5.Models {
 
-    public class Director
-    {
-        public Director()
-        {
-            this.Name = string.Empty;
-            this.Movies = new List<Movie>();
-        }
-        public Director(string d)
-        {
-            this.Name = d;
-            this.Movies = new List<Movie>();
-        }
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        public List<Movie> Movies { get; set; }
+  public class Movie {
+
+    public Movie() {
+      this.Genres = new List<Genre>();
     }
-    public class Genre
-    {
-        public Genre()
-        {
-            this.Name = string.Empty;
-            this.Movies = new List<Movie>();
-        }
-        public Genre(string d)
-        {
-            this.Name = d;
-            this.Movies = new List<Movie>();
-        }
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        public List<Movie> Movies { get; set; }
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public decimal TicketPrice { get; set; }
+
+    public Director Director { get; set; }
+
+    public List<Genre> Genres { get; set; }
+  }
+
+  public class Director {
+
+    public Director() {
+      this.Name = string.Empty;
+      this.Movies = new List<Movie>();
     }
+    public Director(string d) {
+      this.Name = d;
+      this.Movies = new List<Movie>();
+    }
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public List<Movie> Movies { get; set; }
+  }
+  public class Genre {
+
+    public Genre() {
+      this.Name = string.Empty;
+      this.Movies = new List<Movie>();
+    }
+    public Genre(string d) {
+      this.Name = d;
+      this.Movies = new List<Movie>();
+    }
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public List<Movie> Movies { get; set; }
+  }
 }
